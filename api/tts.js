ec4adebbfe367b3306fffb3ecdca9937ce592784
breemo <ibrahim.ali84@hotmail.com>
2025-11-10 10:42:24 +0200

    Update tts.js

--- api/tts.js
+++ api/tts.js
@@ -49,4 +49,4 @@ export default async function handler(req, res) {
     console.error("❌ Server error:", err);
     res.status(500).json({ error: "Server crashed", details: err.message });
   }
-}
+} 
